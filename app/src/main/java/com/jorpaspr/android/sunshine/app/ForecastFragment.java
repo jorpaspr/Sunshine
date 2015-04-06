95c5861803a29fe5606ce4c5bde622490dd23d84
jorpaspr <jorpaspr@gmail.com>
2015-04-06 02:12:32 +0200

    Added DetailActivity with minimal functionality

--- app/src/main/java/com/jorpaspr/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/jorpaspr/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.jorpaspr.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -82,6 +84,16 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = mForecastAdapter.getItem(position);
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT, selectedItem);
+                startActivity(detailIntent);
+            }
+        });
 
         return rootView;
     }
